b6c28af4b522f68be1c7ce6d56fdd02b176a3f8b
jcalvesnunes23-byte <jc.alvesnunes23@gmail.com>
2025-11-11 16:48:21 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>TonIA WhatsApp Bot 🚀</h1>
+      <p>Aplicação implantada com sucesso!</p>
+    </div>
+  );
+}
